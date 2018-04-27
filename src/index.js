2049086437e880cc6538d9172e5bf7ab2cfbb09f
ruuuh20 <pkim2320@gmail.com>
2018-04-27 15:42:05 -0400

    number class

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import _ from 'lodash'
 // registerServiceWorker();
 
 const bgColors = {
+new: 'lightblue',
 playing: '#ccc',
 won: 'green',
 lost: 'red',
@@ -17,10 +18,16 @@ lost: 'red',
 const randomNumberBetween = (min, max) =>
   Math.floor(Math.random() * (max - min + 1)) + min;
 
-  
+
 class Number extends React.Component {
   render() {
-    return <div className="number">{this.props.value}</div>;
+    return (
+    <div
+    className="number"
+    style={{ opacity: this.props.clickable ? 1 : 0.3 }}
+    onClick={() => console.log(this.props.id)}
+       >{this.props.value}</div>
+  )
   }
 }
 
@@ -32,6 +39,9 @@ class Game extends React.Component {
   remainingSeconds: this.props.initialSeconds,
   selectedIds: [],
 };
+
+isNumberAvailable = (numberIndex) =>
+    this.state.selectedIds.indexOf(numberIndex) === -1;
 
   challengeNumbers = Array
     .from({ length: this.props.challengeSize })
@@ -46,15 +56,29 @@ class Game extends React.Component {
         <div className="help">
           Pick 4 numbers that sum to the target in 15 seconds
         </div>
-        <div className="target">42</div>
+        <div
+        className="target"
+        style={{ backgroundColor: Game.bgColors[gameStatus] }}
+        >
+        {this.state.gameStatus === 'new' ? '?' : this.target}
+        </div>
         <div className="challenge-numbers">
         {this.challengeNumbers.map((value, index) =>
-            <Number key={index} value={value} />
+            <Number key={index}
+            id={index}
+            value={this.state.gameStatus === 'new' ? '?' : value}
+            clickable={this.isNumberAvailable(index)} />
            )}
         </div>
         <div className="footer">
-          <div className="timer-value">15</div>
-          <button>Start</button>
+        {this.state.gameStatus === 'new' ? (
+<button>Start</button>
+) : (
+<div className="timer-value">{this.state.remainingSeconds}</div>
+)}
+{['won', 'lost'].includes(this.state.gameStatus) && (
+<button>Play Again</button>
+)}
         </div>
       </div>
     );
